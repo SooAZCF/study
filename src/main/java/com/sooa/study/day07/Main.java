e0bde8a7e8d72cb3d7848314f51d30da2eaf5b0d
SooAZCF <sooazcf@gmail.com>
2023-03-23 15:13:27 +0800

    commit 7

--- src/main/java/com/sooa/study/day07/Main.java
+++ src/main/java/com/sooa/study/day07/Main.java
@@ -2,7 +2,8 @@ package com.sooa.study.day07;
 
 public class Main {
     public static void main(String[] args) {
-        RedBlackMap<String, String> k = new RedBlackMap<>("ss", "tt");
-        System.out.println(k);
+        RedBlackMap<String, String> k = new RedBlackMap<>();
+        k.put("sss", "sst");
+        System.out.println(k.get("sss"));
     }
 }
--- src/main/java/com/sooa/study/day07/RedBlackMap.java
+++ src/main/java/com/sooa/study/day07/RedBlackMap.java
@@ -1,5 +1,6 @@
 package com.sooa.study.day07;
 
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Objects;
 
@@ -7,27 +8,71 @@ public class RedBlackMap<K, V> {
     private transient Entry<K, V> root;
     private transient int size = 0;
 
-    public RedBlackMap(K key, V value) {
-
+    //    构造函数
+    public RedBlackMap() {
     }
 
-    //    put存值
+    //    有key与value对应关联的put
     public V put(K key, V value) {
 //        默认替换旧值
         return put(key, value, true);
     }
 
+    //    没有key与value对应关联的put
+    private V putIfAbsent(K key, V value) {
+        return put(key, value, false);
+    }
+
     private V put(K key, V value, boolean replaceOld) {
         Entry<K, V> temp = root;
-//        判断根节点是否有值
+//        存在根节点
         if (temp == null) {
             root = new Entry<>(key, value, null);
             size = 1;
-            return null;
+            return value;
         }
-//
-        return value;
+//        存在已经存在的根节点
+        Entry<K, V> temParent;//形参父节点
+        int cmp;
+        do {
+            Comparator<? super K> comparator = null;
+//            比较大小
+            temParent = temp;
+            cmp = comparator.compare(key, temParent.key);
+//            切换为左右子节点
+            if (cmp < 0) temParent = temp.left;
+            else if (cmp > 0) temParent = temp.right;
+            else {
+//                返回该key的旧value
+                V oldValue = temp.value;
+                if (replaceOld/*value是否可替代*/ || oldValue == null/*该key对用空value*/) {
+                    temp.value = value;
+                }
+                return oldValue;
+            }
+        } while (temp != null);
+//        存在不存在的根节点
+        Entry<K, V> target = new Entry<>(key, value, temParent);
+        if (cmp < 0) temParent.left = target;
+        else temParent.right = target;
+//        修正红黑颜色
+        fixAfterInsertion(target);
+        return null;
     }
+
+    //    红黑树核心逻辑
+    private void fixAfterInsertion(Entry<K, V> target) {
+
+    }
+
+    private void leftRotate(Entry<K, V> target) {
+
+    }
+
+    private void rightRotate(Entry<K, V> target) {
+
+    }
+
 
     public V get(K key) {
         Entry<K, V> temp = getEntry(key);
@@ -35,7 +80,17 @@ public class RedBlackMap<K, V> {
     }
 
     private Entry<K, V> getEntry(K key) {
+//        树枝遍历
+        Comparable<? super K> keyy = (Comparable<? super K>) key;
+        Entry<K, V> temp = root;
+        while (root != null) {
+            int k = keyy.compareTo(root.key);
+            if (k < 0) temp = root.left;
+            else if (k > 0) temp = root.right;
+            else return root;
+        }
 
+        return null;
     }
 
     //    内部红黑树节点
